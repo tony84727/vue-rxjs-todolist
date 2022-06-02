afa063c10beefffa4c035d3bf96ea31450a17eb6
Tony Duan <tony84727@gmail.com>
2022-06-02 14:55:54 +0000

    refactor: useSubject

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
     <v-main>
-      <ActionPanel @new-todo="newTodo" />
+      <ActionPanel :new-todo="newTodoRef" />
+      <v-btn @click="newTodoRef = { name: 'From Ref' }">
+        Ref
+      </v-btn>
       <List :todos="todos ?? []" />
     </v-main>
   </v-app>
@@ -10,12 +13,10 @@
 <script setup lang="ts">
 import ActionPanel from './components/ActionPanel.vue'
 import List from "./components/List.vue"
-import { useObservable } from "@vueuse/rxjs"
+import { useObservable, useSubject } from "@vueuse/rxjs"
 import { scan, Subject } from 'rxjs';
 import { Todo } from './Todo';
 const newTodo$ = new Subject<Todo>();
-function newTodo(todo: Todo) {
-  newTodo$.next(todo)
-}
+const newTodoRef = useSubject(newTodo$);
 const todos = useObservable(newTodo$.pipe(scan((carry, c) => ([...carry, c]), [] as Todo[])))
 </script>
--- src/components/ActionPanel.vue
+++ src/components/ActionPanel.vue
@@ -2,16 +2,14 @@
 import { Todo } from "../Todo";
 import { ref } from "vue";
 const name = ref('')
-const emit = defineEmits<{
-    (e: 'newTodo', todo: Todo): void
-}>()
+defineProps<{ newTodo: Todo | undefined }>()
 </script>
 
 <template>
     <v-container>
         <v-row align="center">
             <v-text-field label="New Task" v-model="name" />
-            <v-btn @click="emit('newTodo', { name })">
+            <v-btn @click="newTodo = { name }">
                 Add
             </v-btn>
         </v-row>
